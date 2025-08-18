a65438a28ce65dcb14098410c67dd55eb2e112c6
Muhammad Rezky <mrezkysulihin@gmail.com>
2025-08-18 17:33:42 +0800

    Initial Commit

---
+++ SimDevTools/SimDevToolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimDevToolsApp.swift
+//  SimDevTools
+//
+//  Created by Muhammad Rezky on 18/08/25.
+//
+
+import SwiftUI
+
+@main
+struct SimDevToolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SimDevToolsTests/SimDevToolsTests.swift
@@ -0,0 +1,17 @@
+//
+//  SimDevToolsTests.swift
+//  SimDevToolsTests
+//
+//  Created by Muhammad Rezky on 18/08/25.
+//
+
+import Testing
+@testable import SimDevTools
+
+struct SimDevToolsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
