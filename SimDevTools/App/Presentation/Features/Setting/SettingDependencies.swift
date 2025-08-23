bd9c6771240bade0a0a531c62f5b6b364508eda7
Muhammad Rezky <mrezkysulihin@gmail.com>
2025-08-24 00:55:11 +0800

    feat: implement new storage key

--- SimDevTools/App/Presentation/Features/Setting/SettingDependencies.swift
+++ SimDevTools/App/Presentation/Features/Setting/SettingDependencies.swift
@@ -9,8 +9,12 @@ import Foundation
 
 // Setting Storage
 protocol SettingStorage {
+    func loadTargetSimulatorID() -> String?
+    func saveTargetSimulatorID(_ id: String) throws
+    
     func loadAppBundle() -> String?
     func saveAppBundle(_ bundle: String) throws
+    func removeSavedAppBundle() throws
 }
 
 struct UserDefaultsSettingStorage: SettingStorage {
@@ -20,11 +24,23 @@ struct UserDefaultsSettingStorage: SettingStorage {
         self.db = db
     }
     
+    func loadTargetSimulatorID() -> String? {
+        return db.getValue(forKey: .targetSimulatorID)
+    }
+    
+    func saveTargetSimulatorID(_ id: String) throws {
+        return db.save(value: id, forKey: .targetSimulatorID)
+    }
+    
     func loadAppBundle() -> String? {
         return db.getValue(forKey: .selectedAppBundle)
     }
     
     func saveAppBundle(_ bundle: String) throws {
         db.save(value: bundle, forKey: .selectedAppBundle)
+    }
+    
+    func removeSavedAppBundle() throws {
+        db.removeValue(forKey: .selectedAppBundle)
     }
 }
